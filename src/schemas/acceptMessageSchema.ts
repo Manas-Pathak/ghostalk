cbf3f17e0ac3613f45d509093498e83a45573227
Manas-Pathak <pathakmanas111@gmail.com>
2024-09-13 13:28:36 +0530

    Add initial project files

---
+++ src/schemas/acceptMessageSchema.ts
@@ -0,0 +1,6 @@
+import {z} from "zod";
+
+
+export const accetpMessage=z.object({
+   accetpMessages: z.boolean(),
+})
---
+++ src/schemas/signInSchema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+
+export const sighInSchema=z.object({
+    identifier:z.string(),
+    password: z.string(),
+})
